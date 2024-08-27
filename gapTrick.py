09901acc11b4c6cddc5d7f6ad5edca1835fa42ed
Grzegorz Chojnowski <gchojnowski@gmail.com>
2024-08-27 12:18:56 +0200

    fix: disabled models_nums>2 for preds with templates

--- gapTrick.py
+++ gapTrick.py
@@ -885,19 +885,20 @@ def runme(msa_filenames,
         for run_idx in range(1, preds_per_model+1):
             model_name=f"model_{model_idx}"
             if model_name not in list(model_params.keys()):
-                model_params[f"{model_name}_run{run_idx}"] = data.get_model_haiku_params(model_name=model_name+"_ptm", data_dir=data_dir)
+                model_name_local = f"{model_name}_run{run_idx}"
+                model_params[model_name_local] = data.get_model_haiku_params(model_name=model_name+"_ptm", data_dir=data_dir)
                 if model_idx == 1:
                     model_config = config.model_config(model_name+"_ptm")
                     model_config.data.common.num_recycle = num_recycle
                     model_config.model.num_recycle = num_recycle
                     model_config.data.eval.num_ensemble = 1
-                    model_runner_1 = model.RunModel(model_config, model_params[model_name])
+                    model_runner_1 = model.RunModel(model_config, model_params[model_name_local])
                 if model_idx == 3:
                     model_config = config.model_config(model_name+"_ptm")
                     model_config.data.common.num_recycle = num_recycle
                     model_config.model.num_recycle = num_recycle
                     model_config.data.eval.num_ensemble = 1
-                    model_runner_3 = model.RunModel(model_config, model_params[model_name])
+                    model_runner_3 = model.RunModel(model_config, model_params[model_name_local])
 
     is_complex=True
 
